06c6f616c577deaf7516600e0200b1eb9e25e821
PatrickPortes <patrickportes23@hotmail.com>
2022-05-07 05:06:34 -0300

    configuracoes de interface

--- src/main/java/com/dio/santander/banklineapi/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/banklineapi/controller/MovimentacaoController.java
@@ -1,9 +1,11 @@
 package com.dio.santander.banklineapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +35,12 @@ public class MovimentacaoController {
 		return repository.findAll();
 	}
 	
+	//Adaptado para não dar erro, mais abaixo segue o da aula
+	@GetMapping("/{idConta}")
+	public Optional<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+		return repository.findById(idConta);
+	}
+	
 	@PostMapping
 	public void save(@RequestBody NovaMovimentacao movimentacao) {
 		
@@ -40,3 +48,15 @@ public class MovimentacaoController {
 		
 	}
 }
+
+/*
+@GetMapping("/{idConta}")
+public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+	return repository.findByIdConta(idConta);
+} */
+
+
+
+
+
+
--- src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -6,4 +6,6 @@ import com.dio.santander.banklineapi.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
 
+	public List<Movimentacao>findByIdConta(Integer idConta);
+	
 }
